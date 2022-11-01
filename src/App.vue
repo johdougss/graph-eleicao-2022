ec307a68e5a6d53dc1a9a39a49f36fe876eeaf65
johnathansantos <johdougss@gmail.com>
2022-11-01 13:01:35 -0300

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
-<script setup>
-import Dashboard from './components/Dashboard.vue';
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Dashboard2 />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Dashboard2 from './components/Dashboard2.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Dashboard,
+  },
+};
 </script>
 
-<template>
-  <Dashboard />
-</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
